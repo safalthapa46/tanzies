69782827417c4e51f76a98fc54d6fae7d1085cea
safalthapa46 <100404724+safalthapa46@users.noreply.github.com>
2022-11-24 17:52:25 +0545

    tanzies
    game is ready

--- src/App.js
+++ src/App.js
@@ -4,45 +4,114 @@ import Num from "./components/Num"
 
 function App() {
 
-
+  // id generate gareko 
   function randomGenerate() {
     let rand = Math.random();
     return rand;
   }
+
+
   const [dice, setDice] = React.useState(Newdice())  //usestate in newdice
 
-  // creating  random 6 numbers
+
+  const [tanzies, setTanzies] = React.useState(false)  
+  
+
+  React.useEffect(()=>{
+    const all = dice.every(map=> map.isHold)
+    const first= dice[0].value
+    const allValue=dice.every(map=> map.value===first)
+
+    if(all&& allValue){
+      setTanzies(true)
+      console.log("you win!")
+    }
+  },[dice])
+
+
+//select gari sake paxi baki ko number lai resuffle
+  function create() {
+    return{
+      value:Math.ceil(Math.random()*6),
+      isHold:false,
+      id:randomGenerate()
+    }
+  }
+  
+
+
+
+
+
+
+  // creating  random 6 numbers 
   function Newdice() {
     const newArr = []
     for (let i = 0; i < 10; i++) {
       let random = randomGenerate();
-      newArr.push({
-        value: Math.ceil(Math.random() * 6),
-        isHold: false,
-        id: random
-      })
+      newArr.push(create())
     }
     return newArr
   }
+
+
+
+
+
+// line 17 bata create lerako ani map garelo
   function Roll() {
-    setDice(Newdice())
+    if(!tanzies){
+
+      setDice(old => old.map(maps => {
+        return maps.isHold ?
+          maps :
+          create()
+      }))
+    }else{
+      setTanzies(false)
+      setDice(Newdice)
+
+    }
   }
 
+
+
+
+
+
+//eeutai id bhako lai green le hold gareko
   function hold(id) {
-    console.log(id)
+    setDice(old => old.map(map => {
+      return map.id === id ?
+        { ...map, isHold: !map.isHold } :
+        map
+    }))
   }
 
-  const NumElement = dice.map(mapping => <Num value={mapping.value} isHold={mapping.isHold} holds={() => hold(mapping.id)} />)
 
+
+// map gareko
+  const NumElement = dice.map(mapping => <Num value={mapping.value} isHold={mapping.isHold} holds={() => hold(mapping.id)} key={mapping.id} />)
+
+
+
+
+
+
+
+// body
   return (
     <main>
+      <h1 className="head">Tenzies</h1>
+      <p className="rule">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="game">
         {NumElement}
       </div>
-
-      <button className="roll" onClick={Roll}>ROLL</button>
-      <button className="roll" onClick={Roll}>ROLL</button>
-
+      <button 
+       className="roll"
+        onClick={Roll}>
+          {tanzies? "New Game" : "Roll"}
+          </button>
     </main>
   );
 }
